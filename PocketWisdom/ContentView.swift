1df6db920ca6332aabb9a651858f6b1600352318
faist23 <cdfaist@yahoo.com>
2026-04-06 19:46:38 -0400

    Initial Commit

---
+++ PocketWisdom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PocketWisdom
+//
+//  Created by Craig Faist on 4/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PocketWisdom/PocketWisdomApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketWisdomApp.swift
+//  PocketWisdom
+//
+//  Created by Craig Faist on 4/6/26.
+//
+
+import SwiftUI
+
+@main
+struct PocketWisdomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
